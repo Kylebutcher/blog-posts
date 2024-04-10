c6c7348e5f00cf8ef8b8fc6a5f0095079111f172
kyle butcher <kylembutcher97@gmail.com>
2024-04-09 22:58:40 -0500

    changed couple things and added submit button errors

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,9 +1,22 @@
-let link = document.querySelector(".submit");
+let link = document.querySelector(".form");
 let string = localStorage.getItem("submit");
 let submit = JSON.parse(string) || [];
 
 console.log(submit);
-function onSubmit() {
+const usernameInput = document.getElementById('username');
+let title = document.getElementById('blogtitle');
+let blogContent = document.getElementById('blogcontent');
+
+function missingValue(event) {
+  event.preventDefault();
+  if(usernameInput.value == "" || title.value == "" || blogContent == ""){
+alert("Required input in fields");}
+  else {
+    formSubmit();
+  }}
+
+function formSubmit() {
+
   let blogPost = {
     name: document.getElementById('username').value,
     title: document.getElementById('blogtitle').value,
@@ -12,6 +25,7 @@ function onSubmit() {
   submit.push(blogPost);
   console.log(username);
   localStorage.setItem("submit", JSON.stringify(submit));
+  document.location.href="./blog.html";
 }
 
 const element = document.querySelector('body');
@@ -21,3 +35,5 @@ checkBox.addEventListener("click", function () {
   element.classList.toggle("dark-mode");
   console.log("hello");
 })
+
+link.addEventListener("submit", missingValue);
